e5fdfe6d131418241b60e92983ac1b407461ff56
Allwin <allwinselva7@gmail.com>
2026-05-12 15:36:08 +0530

    Fixed bugs

--- app.ts
+++ app.ts
@@ -1,28 +1,24 @@
-import http, { IncomingMessage, ServerResponse } from "http"; 
-// Here we are importing the built-in http module 
+import http, { IncomingMessage, ServerResponse } from "http";
+// Here we are importing the built-in http module
 // and importing the types for request and response objects
 
-const checkingServer = (
-  req: IncomingMessage,
-  res: ServerResponse
-) => {
-
-  console.log(req.method); 
+const checkingServer = (req: IncomingMessage, res: ServerResponse) => {
+  console.log(req.method);
   // From req.method we get the HTTP method like GET, POST, PUT, DELETE...
 
-  console.log(req.headers); 
+  console.log(req.headers);
   // Here we get the request header details
 
-  console.log(req.socket);  
+  console.log(req.socket);
   // Here we get socket/network related information
 
   res.end("Hello Allwin");
 };
 
-const server = http.createServer(checkingServer);  
+const server = http.createServer(checkingServer);
 // Using the http module, we create a server with the createServer method
 
-server.listen(3000, () => { 
+server.listen(3000, () => {
   // The server listens on port 3000.
   // Whenever a request comes to this port,
   // the callback function runs and sends a response.
